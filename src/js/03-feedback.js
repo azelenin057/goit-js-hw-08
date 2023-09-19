9bfca2f2f11cdcf878a14e68a3c1a03c46565124
azelenin057 <yazel057@gmail.com>
2023-09-19 21:12:15 +0300

    Final_FIX

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,11 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
-const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {}; //Решил инициализировать formData таким образом, чтобы не происходила перехапись. Логика: Парсит значение из localStorage, а если его нет то просто {}.
+let formData = loadLocalStorage(); //Вынес в отдельную функцию. Решил инициализировать formData таким образом, чтобы не происходила перехапись. Логика: Парсит значение из localStorage, а если его нет то просто {}.
+
+function loadLocalStorage() {
+  return JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+}
 
 function onFormData(event) {
   formData[event.target.name] = event.target.value;
@@ -15,12 +19,23 @@ function onSubmitForm(event) {
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   event.preventDefault();
   event.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+  //Дописал вот это, я просто в шоке
+  removeLocal();
+  formData = {};
 }
 
+// Додав ще одну функцію
+//==============================================================\\
+function removeLocal() {
+  localStorage.removeItem('feedback-form-state');
+}
+//==============================================================\\
+
 function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const data = loadLocalStorage();
+
   const email = document.querySelector('.feedback-form input');
+
   const message = document.querySelector('.feedback-form textarea');
 
   email.value = data.email || ''; // Добавил проверку чтобы не было undefined
